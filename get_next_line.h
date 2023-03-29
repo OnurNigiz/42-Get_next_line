970ce33dc88876769b2f7cad379076e6b03a1c82
Onur Nigiz <75140593+OnurNigiz@users.noreply.github.com>
2023-03-30 00:17:36 +0300

    utils functions done

--- get_next_line.h
+++ get_next_line.h
@@ -13,7 +13,7 @@ int		ft_strlen(char *c);
 int		ft_find(char *finder);
 char	*ft_line(char *handle_string);
 char	*ft_strjoin(char *lft_str, char *buff);
-// char	*ft_clean(char *handle_string);
+char	*ft_clean(char *handle_string);
 // char	*ft_read(int fd, char *str);
 // char	*get_next_line(int fd);
 
