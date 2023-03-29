73da2d8763ad7a852d8a21f4b54990cbaafb0bcb
Onur Nigiz <75140593+OnurNigiz@users.noreply.github.com>
2023-03-29 23:44:22 +0300

    header & utils init.

---
+++ get_next_line.h
@@ -0,0 +1,20 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 3
+
+# endif
+
+int		ft_strlen(char *c);
+int		ft_find(char *finder);
+char	*ft_line(char *handle_string);
+char	*ft_strjoin(char *lft_str, char *buff);
+// char	*ft_clean(char *handle_string);
+// char	*ft_read(int fd, char *str);
+// char	*get_next_line(int fd);
+
+#endif
