3ac3950b59ccedbfa8c7d966adab41902c7e667b
Onur Nigiz <75140593+OnurNigiz@users.noreply.github.com>
2023-03-31 14:40:56 +0300

    Bonus Added

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <fcntl.h>
-#include "get_next_line.h"
+#include "get_next_line_bonus.h"
+//#include "get_next_line.h"
 #include <stdio.h>
 
 int main(int argc, char **argv)
@@ -7,11 +8,16 @@ int main(int argc, char **argv)
     (void)argc;
     int i = 1;
     char *str;
+    char *str2;
     int fd = open(argv[1], O_RDONLY);
+    int fd2 =open(argv[2], O_RDONLY);
     while (i <= 25)
-    {
+    {   
+        str2 = get_next_line(fd2);
         str = get_next_line(fd);
-        printf("%s", str);
+        printf("%s%s", str, str2);
         i++;
+        free(str);
+        free(str2);
     }
 }
--- test/test_gen.py
+++ test/test_gen.py
@@ -1,4 +1,4 @@
-fd = open("../test.txt", "w")
-for i in range(30):
+fd = open("../test1.txt", "w")
+for i in range(300):
 	fd.write(f"Line number : {i}\n")
 fd.close()
